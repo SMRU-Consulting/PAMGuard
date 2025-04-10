c85411ea0630a0b68fd913d936147c2c29e72a9c
Samuel Tabbutt <st@smruconsulting.com>
2025-04-09 18:23:34 -0700

    Skip .psfx copy in backup  manager (this is a hack, will return later to properly handle .psfx backups)

--- src/backupmanager/stream/FileBackupStream.java
+++ src/backupmanager/stream/FileBackupStream.java
@@ -105,6 +105,9 @@ public abstract class FileBackupStream extends BackupStream {
 			if (lastMod != null && lastMod > maximumTimeMillis) {
 				continue;
 			}
+			if(!aFile.canWrite() || aFile.toString().contains(".psfx")) {
+				continue;
+			}
 			newItems.add(new FileStreamItem(aFile));
 		}
 		long t3 = System.currentTimeMillis();
